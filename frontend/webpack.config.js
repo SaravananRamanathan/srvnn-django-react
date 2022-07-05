7faf6f215add5195480bad86b651ec0f9d560492
Saravanan Ramanathan <srvnnramanathan@gmail.com>
2022-07-05 18:48:53 +0530

    working with configurations

---
+++ frontend/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js", //entery of our code
+  output: {
+    path: path.resolve(__dirname, "./static/frontend"), //output path
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimize: true, //take our js and make it smaller --like getting rid of whitespaes
+  },
+  plugins: [
+    new webpack.DefinePlugin({ //this is doing some sort of optimization
+      "process.env": {
+        // This has effect on the react lib size
+        NODE_ENV: JSON.stringify("production"),
+      },
+    }),
+  ],
+};
